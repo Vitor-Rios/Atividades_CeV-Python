d7ed5f3b7d4ab5b17f7387fb5f2347391464afc2
Vitor Rios <viitor_allves@hotmail.com.br>
2022-07-27 12:31:58 -0300

    teste de restauração

---
+++ Atividade 005.py
@@ -0,0 +1,6 @@
+# Digite um valor e mostre seu Antecessor e Sucessor
+
+valor = int(input("Digite um valor "))
+print(f'O Valor Digitado foi {valor} \n'
+      f'O Antecessor é {valor - 1} \n'
+      f'O Sucessor é {valor + 1}')
---
+++ Atividade 007.py
@@ -0,0 +1,7 @@
+# Digite duas notas e mostre a média
+
+nota1 = float(input("Digite a Primeira Nota:"))
+nota2 = float(input("Digite a Segunda Nota:"))
+med = (nota1 + nota2) / 2
+
+print(f'A Média entre as duas notas é: {med}')
